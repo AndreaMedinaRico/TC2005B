f8f8da05f9e8f944903e98ba49357343348c5358
AndreaMedinaRico <a01705541@tec.mx>
2024-02-22 11:53:05 -0600

    Funcionamiento de botones de cantidad

--- Lab 6/lab6_2.js
+++ Lab 6/lab6_2.js
@@ -1,20 +1,86 @@
+let total_pago = parseInt(document.getElementById('total_pagar').innerText);
+
+const button_suma1 = document.getElementById('suma1');
+const button_resta1 = document.getElementById('resta1');
 const precio_elem1 = parseInt(document.getElementById('precio1').innerText);
+
+button_suma1.onclick = () => {
+    let cant_elem1 = document.getElementById('cant1');
+    cant_elem1 = parseInt(cant1.innerText);
+
+    if (cant_elem1 < 5) {
+        cant_elem1++;
+        cant1.innerText = cant_elem1;
+        total_pago += precio_elem1;
+        total_pagar.innerText = total_pago.toString();
+    }
+    
+}
+
+button_resta1.onclick = () => {
+    let cant_elem1 = document.getElementById('cant1');
+    cant_elem1 = parseInt(cant1.innerText);
+
+    if (cant_elem1 > 0) {
+        cant_elem1--;
+        cant1.innerText = cant_elem1;
+        total_pago -= precio_elem1;
+        total_pagar.innerText = total_pago.toString();
+    }
+}
+
+const button_suma2 = document.getElementById('suma2');
+const button_resta2 = document.getElementById('resta2');
 const precio_elem2 = parseInt(document.getElementById('precio2').innerText);
+
+button_suma2.onclick = () => {
+    let cant_elem2 = document.getElementById('cant2');
+    cant_elem2 = parseInt(cant2.innerText);
+
+    if (cant_elem2 < 5) {
+        cant_elem2++;
+        cant2.innerText = cant_elem2;
+        total_pago += precio_elem2;
+        total_pagar.innerText = total_pago.toString();
+    }
+}
+
+button_resta2.onclick = () => {
+    let cant_elem2 = document.getElementById('cant2');
+    cant_elem2 = parseInt(cant2.innerText);
+
+    if (cant_elem2 > 0) {
+        cant_elem2--;
+        cant2.innerText = cant_elem2;
+        total_pago -= precio_elem2;
+        total_pagar.innerText = total_pago.toString();
+    }
+}
+
+const button_suma3 = document.getElementById('suma3');
+const button_resta3 = document.getElementById('resta3');
 const precio_elem3 = parseInt(document.getElementById('precio3').innerText);
 
-const cant_elem1 = document.getElementById('input_cant_elem1').value;
-const cant_elem2 = document.getElementById('input_cant_elem2').value;
-const cant_elem3 = document.getElementById('input_cant_elem3').value;
+button_suma3.onclick = () => {
+    let cant_elem3 = document.getElementById('cant3');
+    cant_elem3 = parseInt(cant3.innerText);
 
+    if (cant_elem3 < 5) {
+        cant_elem3++;
+        cant3.innerText = cant_elem3;
+        total_pago += precio_elem3;
+        total_pagar.innerText = total_pago.toString();
+    }
+}
 
-const total_pago = parseInt(document.getElementById('total_pagar').value);
+button_resta3.onclick = () => {
+    let cant_elem3 = document.getElementById('cant3');
+    cant_elem3 = parseInt(cant3.innerText);
 
-const btn_total = document.getElementById('botton_total');
-
-btn_total.onclick = () => {
-    let total = 0;
-    total = (precio_elem1 * cant_elem1) + (precio_elem2 * cant_elem2) + (precio_elem3 * cant_elem3);
-    console.log(precio_elem1, cant_elem1, precio_elem2, cant_elem2, precio_elem3, cant_elem3)
-    console.log(total);
-    total_pago.innerText = total;
+    if (cant_elem3 > 0) {
+        cant_elem3--;
+        cant3.innerText = cant_elem3;
+        total_pago -= precio_elem3;
+        total_pagar.innerText = total_pago.toString();
+    }
 }
