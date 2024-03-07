f9454fea11d4347e442559d5e996484d79ac30f7
AndreaMedinaRico <a01705541@tec.mx>
2024-03-06 20:46:21 -0600

    Creaciòn de gtml

--- Lab 11/app.js
+++ Lab 11/app.js
@@ -2,6 +2,11 @@ const filesystem = require('fs');
 const express = require('express');
 const app = express();
 
+const path = require('path');
+    // express.static() --> archivos estáticos --> html, css, img
+    // __dirname --> variable de RUTA del directorio
+app.use(express.static(path.join(__dirname, 'public')));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
