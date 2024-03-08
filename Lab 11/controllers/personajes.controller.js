ce601b9465f9c29bcc92dfbe3b1a3adf506294d9
AndreaMedinaRico <a01705541@tec.mx>
2024-03-08 13:49:49 -0600

    Arreglar personajes/secundarios

--- Lab 11/controllers/personajes.controller.js
+++ Lab 11/controllers/personajes.controller.js
@@ -1,10 +1,10 @@
 exports.getPersonajes = (request, response, next) => {
     response.render('personajes.ejs');
-}
+};
 
 exports.getSecundarios = (request, response, next) => {
     response.render('secundarios.ejs');
-}
+};
 
 exports.get404 = (request, response, next) => {
     response.status(404);
