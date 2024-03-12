73d9620b3c56a7c43a556b459de8b2022356304f
AndreaMedinaRico <a01705541@tec.mx>
2024-03-12 12:59:49 -0600

    bscrypt contraseña en bd cifrada

--- Lab 11/models/usuario.model.js
+++ Lab 11/models/usuario.model.js
@@ -1,4 +1,5 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
 
 module.exports = class Usuario {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
@@ -10,10 +11,16 @@ module.exports = class Usuario {
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute(
+        // Promesa
+        return bcrypt.hash(this.password, 12)       // Encriptar 12 veces
+        .then((passwordCifrado) => {
             `INSERT INTO usuario (username, nombre, password) 
             VALUES (?, ?, ?)`, 
-            [this.username, this.nombre, this.password]);
+            [this.username, this.nombre, passwordCifrado]
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
     
     static fetchOne(username) {
