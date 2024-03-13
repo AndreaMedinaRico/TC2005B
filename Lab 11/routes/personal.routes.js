d8384bcdae9810a115755d8e7e3848b9bb24bf40
AndreaMedinaRico <a01705541@tec.mx>
2024-03-12 21:03:34 -0600

    Protecci+on rutas personal

--- Lab 11/routes/personal.routes.js
+++ Lab 11/routes/personal.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');      // Importar middleware de autenticación
 
 const personalController = require('../controllers/personal.controller.js');
 
-router.get('/', personalController.getPersonal);
-router.post('/', personalController.postPersonal);
-router.get('/:idPersona', personalController.postPersonal);
+router.get('/', isAuth, personalController.getPersonal);
+router.post('/', isAuth, personalController.postPersonal);
+router.get('/:idPersona', isAuth, personalController.postPersonal);
 router.use(personalController.get404);
 
 module.exports = router;
